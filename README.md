e1394cadf55b322595ee10f2b66dfad634116332
Danny Abukalam <danny@softiron.com>
2021-02-01 13:26:22 -0500

    Initial Commit of RGW Usage Example script
    Signed-off-by: Danny Abukalam <danny@softiron.com>

---
+++ README.md
@@ -0,0 +1,34 @@
+# Examples for using the RGW Admin API
+
+## Installation
+
+    pip install rgw-usage-example/
+
+## Usage
+
+Drop a `keys.json` file in the repo that looks like this:
+
+    {
+      "uid": "alice",
+      "access_key": "RTZREJDAB4VRCCZF2SJN",
+      "secret_key": "pzLq6MjprdyI21UjXvBm6N5MgmhWeswaPR7PmwPX"
+    }
+
+The above user will need admin caps on the rados gateway, which you can assign with the following commands:
+
+    radosgw-admin caps add --uid="alice" --caps="users=*"
+    radosgw-admin caps add --uid="alice" --caps="buckets=*"
+    radosgw-admin caps add --uid="alice" --caps="metadata=*"
+    radosgw-admin caps add --uid="alice" --caps="usage=*"
+
+You should then be able to pass a hostname and port as an argument:
+
+    $ ./usage_example.py
+    Usage: ./usage_example.py RGW_HOSTNAME:PORT
+
+For example:
+
+    $ ./usage_example.py 192.168.3.99
+    danny           {'bytes_sent': 3080, 'bytes_received': 11534336000, 'ops': 749, 'successful_ops': 744}
+    alice           {'bytes_sent': 1038, 'bytes_received': 1048576000, 'ops': 73, 'successful_ops': 72}
+    bob             No Usage
---
+++ usage_example.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+#
+# Copyright 2020 SoftIron
+# Example script to extract per-user usage statistics from RGW
+
+import json
+import os
+import radosgw
+import sys
+
+def _load_keys(filename):
+
+    try:
+        with open(filename) as json_file:
+            data = json.load(json_file)
+            secret_key = data['secret_key']
+            access_key = data['access_key']
+            return (secret_key, access_key)
+    except:
+        print("Unable to read keys from file: " + filename)
+        exit(-1)
+
+
+
+if __name__ == "__main__":
+
+    # Check if keys.json file exists in current working directory
+    if os.path.isfile('keys.json'):
+        secret_key, access_key = _load_keys('keys.json')
+    else:
+        print("Keys file not found")
+        exit(-1)
+
+    # Check that we've received a single argument
+    if len(sys.argv) != 2:
+        print("Usage: {} RGW_HOSTNAME:PORT".format(sys.argv[0]))
+        exit(-1)
+
+    # Split address
+    address = sys.argv[1].split(':')
+
+    # Use default port of 7480 if no port specified
+    if len(address) == 1:
+        address.append(7480)
+
+    # Open connection to RGW
+    rgw = radosgw.connection.RadosGWAdminConnection(host=address[0],
+                                                         port=address[1],
+                                                         access_key=access_key,
+                                                         secret_key=secret_key,
+                                                         is_secure=False)
+
+    # Fetch list of UIDs
+    users = rgw.get_uids()
+
+    # Print usage for each user
+    for uid in users:
+        usage = rgw.get_usage(uid=uid)
+
+        try:
+            totals = usage["summary"][0]["total"]
+        except:
+            totals = "No Usage"
+
+        print("{}\t\t{}".format(uid,totals))
+
